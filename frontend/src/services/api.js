653113701066d6f876d3ffba1376984c08084938
João Victor <54009514+T4SUYA@users.noreply.github.com>
2020-01-09 14:47:54 -0300

    cc

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000' || 'https://shopping-cart-jv.herokuapp.com' ,
+    baseURL: process.env.REACT_APP_API || 'http://localhost:5000',
     timeout: 6000,
     headers: { 'Content-type' : 'application/json' }
 })
